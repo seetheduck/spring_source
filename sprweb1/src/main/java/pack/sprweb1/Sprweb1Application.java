3d185d1b155961e5d1b92c7c4769b603c28a4942
seetheduck <lwj0506@naver.com>
2024-07-10 17:34:43 +0900

    2024-07-10
    Spring Boot 시작

---
+++ sprweb1/src/main/java/pack/sprweb1/Sprweb1Application.java
@@ -0,0 +1,13 @@
+package pack.sprweb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb1Application.class, args);
+	}
+
+}
---
+++ sprweb1/src/test/java/pack/sprweb1/Sprweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pack.sprweb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprweb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
